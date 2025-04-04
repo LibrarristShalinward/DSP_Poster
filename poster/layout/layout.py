cf655b5e5e691719538d3a66544304e84c1cf41d
LibrarristShalinward <jbh23@mails.tsinghua.edu.cn>
2025-04-05 01:58:53 +0800

    实现了基础版的layout

---
+++ poster/layout/layout.py
@@ -0,0 +1,343 @@
+# region 格式要求
+# docstring要写成这种形式：
+# """
+# Generate a sequence of points along the parametric elliptical curve.
+
+# Parameters:
+#     a (np.ndarray): Point a, shape (..., 2,).
+#     b (np.ndarray): Point b, shape (..., 2,).
+#     c (np.ndarray): Point c, shape (..., 2,).
+
+# Returns:
+#     np.ndarray: Sequence of points, shape (..., N, 2).
+# """
+# 
+# 运算符（=等）左右不允许直接连接变量名、数字或参数名，须以空格隔开
+# endregion
+
+import yaml
+from os.path import dirname, abspath, join
+from .utils import func2getitem, method2geitem
+
+# 我需要用matplotlib绘制一幅由正方形图标和与x、y轴平行的线段组成的折线连接线组成的海报。其中，图标数量已知，以给定的、与其数量匹配的行数和列数呈矩形阵列排列，其横纵间距与穿行其间的连接线数量有关。
+# 在本文件中，我暂时不考虑图片绘制的具体实现，而是先创建一个布局类Layout，以计算不同图片和连接线的位置
+# 首先，本文件需要导入一个位于同一文件夹下的.yaml文件，该文件用于描述内容示例如下：
+# ```yaml
+# icon: 
+#   size: .6 # 图标大小
+#   border: .1 # 第一行/最后一行图标与海报边界的间距
+# connection: 
+#   gap: .1 # 连接线间距
+#   gap2icon: .2 # 连接线与图标的额外间距(连接线与图标间距等于该值+gap/2)
+#   gap2border: .1 # 连接线与海报边界的额外间距(连接线与海报边界间距等于该值+gap/2)
+# ```
+class LayoutConfig: 
+    """
+    布局配置类，用于读取布局配置文件并存储相关参数。
+    """
+    def __init__(self, file_path): 
+        # region 基本属性
+        with open(file_path, 'r', encoding = 'utf-8') as file:
+            yml = yaml.safe_load(file)
+        self.icon_size: float = yml['icon']['size']
+        """图标大小"""
+        self.iborder: float = yml['icon']['border']
+        """第一行/最后一行图标与海报边界的间距"""
+        self.cgap: float = yml['connection']['gap']
+        """连接线间距"""
+        self.cgap2icon: float = yml['connection']['gap2icon']
+        """连接线与图标的额外间距(连接线与图标间距等于该值+gap/2)"""
+        self.cgap2border: float = yml['connection']['gap2border']
+        """连接线与海报边界的额外间距(连接线与海报边界间距等于该值+gap/2)"""
+        # endregion
+
+        # region 计算属性: 间距
+        # endregion
+
+        # region 计算属性: 图标坐标
+        # endregion
+
+        # region 计算属性: 连接线节点坐标相关变量
+        self.con_cl0_pos0 = self.cgap2border + self.cgap / 2.
+        """第一列图标左边的簇中第一条的x坐标/第一行图标下方的簇中第一条的y坐标"""
+        self.con_pos0 = self.cgap2icon + (self.icon_size + self.cgap) / 2.
+        """图标右侧纵向连接线簇最左边一条与其中心距离"""
+        # endregion
+cfg = LayoutConfig(
+    join(dirname(abspath(__file__)), 'config.yaml')
+)
+
+
+
+class Layout:
+    """
+    Layout类用于计算图标和连接线的位置。
+    图标阵列外，只有左右有连接线，上方无连接线。
+    后续坐标以图片左上角为零点，向右和向上为正方向。
+    """
+    def __init__(self, 
+                icon_row: int, 
+                icon_col: int,
+                inner_con_cap: tuple[int, int], 
+                outer_con_cap: tuple[int, int], 
+                cfg: LayoutConfig = cfg
+            ): 
+        """
+        Layout类用于计算图标和连接线的位置。
+
+        Parameters:
+            icon_num (int): 图标行数
+            icon_col (int): 图标列数
+            inner_con_cap (tuple[int, int]):
+                - 左右相邻图标之间的连接线数量
+                - 上下相邻图标之间的连接线数量/最后一列下方连接线数量
+            outer_con_cap (tuple[int, int]):
+                - 最左一列图标与左边界之间的连接线数量
+                - 最右一列图标与右边界之间的连接线数量
+            cfg (LayoutConfig): 布局配置类，默认为cfg
+        """
+        # region 基本属性
+        self.nrow = icon_row
+        """图标行数"""
+        self.ncol = icon_col
+        """图标列数"""
+        self.cfg = cfg
+        """布局配置类"""
+        self.inner_con_cap = inner_con_cap
+        """左右和上下相邻图标之间的连接线数量"""
+        self.outer_con_cap = outer_con_cap
+        """图标与左右边界之间的连接线数量"""
+        # endregion
+
+        # region 计算属性: 间距
+        self.icon_gap = (
+            self.inner_con_cap[0] * self.cfg.cgap + 2 * self.cfg.cgap2icon, 
+            self.inner_con_cap[1] * self.cfg.cgap + 2 * self.cfg.cgap2icon
+        )
+        """图标(左右, 上下)间距"""
+
+        # endregion
+
+        # region 计算属性: 图标坐标
+        self.icon_pos0 = (
+            self.cfg.cgap2border + self.cfg.cgap * self.outer_con_cap[0] + self.cfg.cgap2icon + self.cfg.icon_size / 2., 
+            - (self.cfg.iborder + self.cfg.icon_size / 2.)
+            # 为什么取负？因为y上为正方向，但图标向下排列，所以y坐标要取负
+        )
+        """左上角图标中心点坐标"""
+        self.icon_step = (
+            self.icon_gap[0] + self.cfg.icon_size, 
+            - (self.icon_gap[1] + self.cfg.icon_size)
+        )
+        """相邻图标中心点间距"""
+        # endregion
+
+        # region 计算属性: 连接线节点坐标相关变量
+        # endgion
+
+        # 图像大小
+        self.fig_size = (
+            self.con_pos_x[self.ncol][-1] + self.cfg.cgap / 2 + self.cfg.cgap2border, 
+            - self.con_pos_y[self.nrow - 1][-1] + self.cfg.cgap / 2 + self.cfg.cgap2border, 
+        )
+    
+    # region 可索引属性：坐标
+    @property
+    @method2geitem
+    def icon_pos(self, idx: tuple[int, int]) -> tuple[float, float]:
+        """
+        获取图标中心点坐标
+
+        Parameters:
+            idx tuple[int, int]: 图标索引(行号, 列号)
+
+        Returns:
+            tuple[float, float]: 图标中心点坐标
+        """
+        row, col = idx
+        # 无效索引
+        if row >= self.nrow or row < 0: 
+            raise IndexError(f"Row index {row} out of range (0-{self.nrow})")
+        if col >= self.ncol or col < 0:
+            raise IndexError(f"Column index {col} out of range (0-{self.ncol})")
+        # 计算坐标
+        return (
+            self.icon_pos0[0] + col * self.icon_step[0], 
+            self.icon_pos0[1] + row * self.icon_step[1]
+        )
+    
+    @property
+    @method2geitem
+    def _clust_pos_x0(self, idx: int) -> float:
+        """
+        获取纵向连接线簇中最左一条的x坐标
+
+        Parameters:
+            idx (int): 最左一列图标与左边界之间的连接线为第-1簇，第一列图标右侧的连接线为第0簇……最右一列图标与右边界之间的连接线为第ncol - 1簇，再向右的一簇为ncol簇。
+        Returns:
+            float: 簇中最左一条的x坐标
+        """
+        if idx == -1: #第一列图标左边的簇
+            return self.cfg.con_cl0_pos0
+        elif idx >= 0 and idx < self.ncol: # 两列图标之间的簇及最右边一列图标右边的簇
+            return self.icon_pos[0, idx][0] + self.cfg.con_pos0
+        elif idx == self.ncol: 
+            return self._clust_pos_x0[self.ncol - 1] + self.cfg.cgap * self.inner_con_cap[0]
+        else: # 无效索引
+            raise IndexError(f"Column index {idx} out of range (-1-{self.ncol})")
+    
+    @property
+    @method2geitem
+    def con_pos_x(self, idx: int) -> func2getitem[int, float]:
+        """
+        获取连接线纵向部分x坐标
+
+        Parameters:
+            idx (int): 同_clust_pos_x
+        Returns:
+            float: 能返回对应簇内所有x坐标的可索引对象
+        """
+        x0 = self._clust_pos_x0[idx] # 簇中最左一条的x坐标+cl_idx合法性检查
+        if idx == -1:
+            n_con = self.outer_con_cap[0] # 第一列图标左边的簇
+        elif idx >= 0 and idx < self.ncol: 
+            n_con = self.inner_con_cap[0]
+        else: # 最右边一列图标右边的簇
+            n_con = self.outer_con_cap[1]
+        @func2getitem
+        def wrapper(idx: int) -> float: 
+            """
+            获取连接线纵向部分x坐标
+
+            Parameters:
+                idx (int): 每一簇最左边为第0条，该索引可以类似list的索引取包括负值的任何整数值，-1表示最后一条连接线，依此类推。
+            Returns:
+                float: 连接线x坐标
+            """
+            return x0 + (idx % n_con) * self.cfg.cgap
+        return wrapper
+    
+    @property
+    @method2geitem
+    def con_pos_y(self, idx: int) -> func2getitem[int, float]: 
+        """
+        获取连接线横向部分y坐标
+
+        Parameters:
+            idx (int): 第几簇连接线。第一行图标下方的连接线为第0簇……最后一行图标下方为第nrow-1簇。
+        Returns:
+            float: 能返回对应簇内所有y坐标的可索引对象
+        """
+        # 合法性检查
+        if idx >= self.nrow or idx < 0:
+            raise IndexError(f"Row index {idx} out of range (0-{self.nrow - 1})")
+        y0 = self.icon_pos[idx, 0][1] - self.cfg.con_pos0
+        @func2getitem
+        def wrapper(idx: int) -> float: 
+            """
+            获取连接线纵向部分y坐标
+
+            Parameters:
+                idx (int): 每一簇最上方为第0条，该索引可以类似list的索引取包括负值的任何整数值，-1表示最后一条连接线，依此类推。
+            Returns:
+                float: 连接线y坐标
+            """
+            return y0 - (idx % self.inner_con_cap[1]) * self.cfg.cgap
+        return wrapper
+    
+    @property
+    @method2geitem
+    def _con_icon_node(self, idx: tuple[int, int]) -> float:
+        """
+        获取连接线从图标上方或下方引出时相对图标中心的x坐标
+
+        Parameters:
+            idx (int): (将引出几条线，要获取第几条线的坐标)
+        Returns:
+            float: 连接线从图标上方或下方引出时相对图标中心的x坐标
+        """
+        return self.cfg.cgap * (idx[1] + (idx[0] - 1) / 2.)
+    
+    @property
+    @method2geitem
+    def con_start(self, icon_idx: tuple[int, int]) -> func2getitem[tuple[int, int], tuple[float, float]]: 
+        """
+        获取连接线从图标引出点的坐标
+
+        Parameters:
+            icon_idx (tuple[int, int]): 同icon_pos索引
+        Returns:
+            func2getitem[tuple[int, int], tuple[float, float]]: 一个可索引具体坐标的对象
+        """
+        x0, y0 = self.icon_pos[icon_idx] # 图标中心坐标
+        y0 -= self.cfg.icon_size / 2. # 图标下方引出连接线时，y坐标要减去图标半径
+        @func2getitem
+        def wrapper(idx: tuple[int, int]) -> tuple[float, float]: 
+            """
+            获取连接线从图标引出点的坐标
+
+            Parameters:
+                idx (tuple[int, int]): 同_con_icon_node索引
+            Returns:
+                tuple[float, float]: 连接线从图标引出点的坐标
+            """
+            return x0 + self._con_icon_node[idx], y0
+        return wrapper
+
+    @property
+    @method2geitem
+    def con_end(self, icon_idx: tuple[int, int]) -> func2getitem[tuple[int, int], tuple[float, float]]: 
+        """
+        获取连接线到达图标点的坐标
+
+        Parameters:
+            icon_idx (tuple[int, int]): 同icon_pos索引
+        Returns:
+            func2getitem[tuple[int, int], tuple[float, float]]: 一个可索引具体坐标的对象
+        """
+        x0, y0 = self.icon_pos[icon_idx] # 图标中心坐标
+        y0 += self.cfg.icon_size / 2. # 图标上方引出连接线时，y坐标要加上图标半径
+        @func2getitem
+        def wrapper(idx: tuple[int, int]) -> tuple[float, float]: 
+            """
+            获取连接线到达图标点的坐标
+
+            Parameters:
+                idx (tuple[int, int]): 同_con_icon_node索引
+            Returns:
+                tuple[float, float]: 连接线从图标引出点的坐标
+            """
+            return x0 + self._con_icon_node[idx], y0
+        return wrapper
+    # endregion
+
+
+# .utils内容: 
+# ```python
+# """
+# 一些工具函数
+# """
+# from typing import Callable, Generic, TypeVar
+# T = TypeVar('T') # 索引类型
+# RT = TypeVar('RT') # 返回值类型
+# class func2getitem(Generic[T, RT]):
+#     """
+#     将一个函数转换为可以使用索引访问的对象
+#     """
+#     def __init__(self, func: Callable[[T], RT]) -> None:
+#         self.func = func
+
+#     def __getitem__(self, idx: T) -> RT:
+#         return self.func(idx)
+# S = TypeVar('S') # 相当于self的类型
+# def method2geitem(func: Callable[[S, T], RT]) -> Callable[[S], func2getitem[T, RT]]: 
+#     """
+#     将一个方法转换为可以使用索引访问的对象
+#     """
+#     def wrapper_for_property(self_: S) -> func2getitem[T, RT]: 
+#         @func2getitem
+#         def wrapper(idx: T) -> RT:
+#             return func(self_, idx)
+#         return wrapper
+#     return wrapper_for_property
+# ```
